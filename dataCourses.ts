4f60592c27856001dd167c228493f5a3f90db4a5
Felipe Duque <jf.duque@uniandes.edu.co>
2020-04-04 10:09:04 -0500

    Taller TS

---
+++ dataCourses.ts
@@ -0,0 +1,10 @@
+
+import { Course } from './course.js';
+
+export const dataCourses = [
+  new Course("Desarrollo Sw", "Jhonatan", 3),
+  new Course("Futbol 1", "Freddy Rincón", 3),
+  new Course("Algoritmos", "Carlos Fuentes", 3),
+  new Course("Estructuras de Datos", "Yesid D", 3),
+  new Course("Futbol 2", "James R", 3)
+]
---
+++ scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo Sw", "Jhonatan", 3),
+    new Course("Futbol 1", "Freddy Rincón", 3),
+    new Course("Algoritmos", "Carlos Fuentes", 3),
+    new Course("Estructuras de Datos", "Yesid D", 3),
+    new Course("Futbol 2", "James R", 3)
+];
